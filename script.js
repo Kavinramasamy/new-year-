b0ac76975e8a7f777cb11049272b2731a9c32556
Kavinramasamy <124972388+Kavinramasamy@users.noreply.github.com>
2023-03-21 13:39:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var new_para=document.createElement("p");
+new_para.innerHTML="10";
+document.body.append(new_para);
+var count=9;
+function timing(){
+    if(count>0){
+        new_para.innerHTML=count;
+        count--;
+    }
+    else{
+        new_para.innerHTML="Happy New Year...❣";
+        clearInterval(a);
+    }
+}
+var a=setInterval(timing,1000);
